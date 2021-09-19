1c4faf9458c3589e8c40c8310fd2e5186ea1b10e
will-cesar <willcesar010421@gmail.com>
2021-09-19 11:08:01 -0300

    aula 20 - Estilização do navbar e do footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 import Home from "./components/pages/Home";
 import Company from "./components/pages/Company";
@@ -6,21 +6,23 @@ import Contact from "./components/pages/Contact";
 import NewProject from "./components/pages/NewProject";
 
 import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Projects from "./components/pages/Projects";
 
 function App() {
   return (
     <Router>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/contact">Contato</Link>
-        <Link to="/company">Empresa</Link>
-        <Link to="/newproject">Novo projeto</Link>
-      </div>
+      <Navbar />
 
       <Switch>
         <Container customClass="min-height">
           <Route exact path="/">
             <Home />
+          </Route>
+
+          <Route exact path="/projects">
+            <Projects />
           </Route>
 
           <Route exact path="/company">
@@ -36,6 +38,8 @@ function App() {
           </Route>
         </Container>
       </Switch>
+
+      <Footer />
     </Router>
   );
 }
