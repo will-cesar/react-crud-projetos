90fe874c152a08b12411045da31f95935a7cf42b
will-cesar <willcesar010421@gmail.com>
2021-09-21 23:23:28 -0300

    Aula 26 - criação dos estilos para exibir os projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,10 @@
 import { useLocation } from "react-router-dom";
+
+import Container from "../layout/Container";
+import LinkButton from "../layout/LinkButton";
 import Message from "../layout/Message";
+
+import styles from "./Projects.module.css";
 
 function Projects() {
   const location = useLocation();
@@ -10,9 +15,17 @@ function Projects() {
   }
 
   return (
-    <div>
-      <h1>Meus projetos</h1>
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus projetos</h1>
+        <LinkButton to="/newproject" text="Criar projeto" />
+      </div>
+
       {message && <Message msg={message} type="success" />}
+
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
     </div>
   );
 }
