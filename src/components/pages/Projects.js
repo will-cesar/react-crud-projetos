139fd529097751cd878cf8d74df9eaa9b305511f
will-cesar <willcesar010421@gmail.com>
2021-09-22 20:28:54 -0300

    Aula 27 - customizando a lista de projetos

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,18 +1,36 @@
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 import Container from "../layout/Container";
 import LinkButton from "../layout/LinkButton";
 import Message from "../layout/Message";
+import ProjectCard from "../project/ProjectCard";
 
 import styles from "./Projects.module.css";
 
 function Projects() {
+  const [projects, setProjects] = useState([]);
+
   const location = useLocation();
   let message = "";
 
   if (location.state) {
     message = location.state.message;
   }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProjects(data);
+      })
+      .catch((err) => console.log(err));
+  }, []);
 
   return (
     <div className={styles.project_container}>
@@ -24,7 +42,16 @@ function Projects() {
       {message && <Message msg={message} type="success" />}
 
       <Container customClass="start">
-        <p>Projetos...</p>
+        {projects.length > 0 &&
+          projects.map((project) => (
+            <ProjectCard
+              id={project.id}
+              name={project.name}
+              budget={project.budget}
+              category={project.category.name}
+              key={project.id}
+            />
+          ))}
       </Container>
     </div>
   );
