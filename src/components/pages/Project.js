29ce2ee36e93d3575b6ce663649897b676b4df41
will-cesar <willcesar010421@gmail.com>
2021-10-25 21:33:03 -0300

    Aula 36 - Excluindo o serviço

--- src/components/pages/Project.js
+++ src/components/pages/Project.js
@@ -1,4 +1,4 @@
-import { parse, v4 as uuidv4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -95,7 +95,33 @@ function Project() {
       .catch((err) => console.log(err));
   }
 
-  function removeService() {}
+  function removeService(id, cost) {
+    setMessage("");
+
+    const servicesUpdated = project.services.filter(
+      (service) => service.id !== id
+    );
+
+    const projectUpdated = project;
+
+    projectUpdated.services = servicesUpdated;
+    projectUpdated.cost = parseFloat(projectUpdated.cost) - parseFloat(cost);
+
+    fetch(`http://localhost:5000/projects/${projectUpdated.id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(projectUpdated),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setProject(projectUpdated);
+        setServices(servicesUpdated);
+        setMessage("Serviço removido com sucesso!");
+      })
+      .catch((err) => console.log(err));
+  }
 
   function toggleProjectForm() {
     setShowProjectForm(!showProjectForm);
--- src/components/service/ServiceCard.js
+++ src/components/service/ServiceCard.js
@@ -3,7 +3,10 @@ import styles from "../project/ProjectCard.module.css";
 import { BsFillTrashFill } from "react-icons/bs";
 
 function ServiceCard({ id, name, cost, description, handleRemove }) {
-  const remove = (e) => {};
+  const remove = (e) => {
+    e.preventDefault();
+    handleRemove(id, cost);
+  };
 
   return (
     <div className={styles.project_card}>
